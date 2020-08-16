a7178b083dc2c95cd906a603bec9042fdd584ef9
sl2101 <67243351+sl2101@users.noreply.github.com>
2020-08-16 21:36:33 +0100

    added all files

---
+++ project.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function (){
+
+    //var photo = doument.getElementById("click-me");
+    //photo.addEventListener("click", function (){
+        // var paragraph = document.getElementById("find-me");
+        // paragraph.classList.toggle("hidden");
+
+
+
+
+
+    });
+});
+
